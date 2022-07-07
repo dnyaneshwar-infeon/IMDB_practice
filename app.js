cc99105a7d34e35ab4b49065af2cbbcac61d2414
unknown <dnyaneshwar.jadhav@infeon.in>
2022-07-07 13:13:40 +0530

    new

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const con=require('./util/connection');
 const insert_actor=require('./controller/all');
 const insert_movie=require('./controller/all');
 const insert_producer=require('./controller/all');
-const asso_movie_producer=require('./controller/all');
 const asso_movie_actor=require('./controller/all');
+const movie_producer=require('./controller/all');
+const insert_fan=require('./controller/all');
+const give_rating=require('./controller/all');
+const get_rating=require('./controller/all');
 
 
 app.post('/insert_actor' ,insert_actor.insert_actor);
 app.post('/insert_movie' ,insert_movie.insert_movie);
 app.post('/insert_producer',insert_producer.insert_producer);
 app.post('/asso_movie_actor' , asso_movie_actor.asso_movie_actor);
-app.post('/asso_movie_producer' ,asso_movie_producer.asso_movie_producer);
+app.post('/movie_producer',movie_producer.insert_producer);
+app.post('/insert_fan' ,insert_fan.insert_fan);
+app.post('/give_rating' , give_rating.give_rating);
+app.get('/get_rating' , get_rating.get_rating);
 
 app.listen(8000 , ()=>
 {console.log(`Listening on Port ${8000}`);}
--- controller/all.js
+++ controller/all.js
@@ -1,3 +1,4 @@
+const { execFileSync } = require("child_process");
 const con = require("../util/connection");
 
 const insert_actor=function(req , res)
@@ -74,12 +75,45 @@ const asso_movie_actor=function(req ,res)
         }
     })
 }
-const asso_movie_producer=function(req , res)
+const movie_producer=function(req , res)
 {
-    var pro=req.query.pro_id;
     var movie=req.query.movie_id;
-    var sql="insert into movie_producer(pro_id , movie_id)values(?,?)";
-    con.query(sql , [pro , movie] , function(error,result)
+    var pro=req.query.pro_id;
+    var sql="insert into movie_producer(movie_id , pro_id)values(?,?)";
+    con.query(sql ,[movie,pro],function(error , resu)
+    {
+        if(error)
+        {
+            throw error;
+        }
+        else{
+            res.json(result);
+        }
+    })
+
+}
+const insert_fan=function(req , res)
+{
+    var uname=req.query.user_name;
+    var sql="insert into fan(user_name)values(?)";
+    con.query(sql,[uname],function(error,result){
+        if(error)
+        {
+            throw error;
+        }
+        else
+        {
+            res.json(result);
+        }
+    })
+}
+const give_rating=function(req,res)
+{
+    var user=req.query.user_id;
+    var movie=req.query.movie_id;
+    var rating=req.query.rating;
+    var sql="insert into fan_rating(user_id , movie_id , rating)values(?,?,?)";
+    con.query(sql ,[user,movie,rating],function(error,result)
     {
         if(error)
         {
@@ -92,8 +126,27 @@ const asso_movie_producer=function(req , res)
     })
 
 }
+const get_rating=function(req,res)
+{
+    var movie=req.query.movie_id;
+    var sql="select avg(rating) from (select rating from fan join fan_rating on fan.user_id=fan_rating.user_id join movies on movies.movie_id=fan_rating.movie_id where movies.movie_id=?) as rating";
+    con.query(sql , [movie],function(error , result)
+    {
+        if(error)
+        {
+            throw error;
+        }
+        else
+        {
+            res.json(result);
+        }
+    })
+}
 module.exports.insert_actor=insert_actor;
 module.exports.insert_movie=insert_movie;
 module.exports.insert_producer=insert_producer;
 module.exports.asso_movie_actor=asso_movie_actor;
-module.exports.asso_movie_producer=asso_movie_actor;
+module.exports.movie_producer=movie_producer;
+module.exports.insert_fan=insert_fan;
+module.exports.give_rating=give_rating;
+module.exports.get_rating=get_rating;
